0dc1e0e010b89f4ea03ce4cdab79e8e1d364faf9
taqiyyaghazi <taqiyyaghazi@gmail.com>
2024-01-06 13:23:30 +0700

    feat: create tagline component

---
+++ src/components/Tagline.tsx
@@ -0,0 +1,25 @@
+import { inter } from '@/lib/fonts';
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const Tagline = () => {
+  return (
+    <div
+      className={`${inter.className} text-rumah-peran-red bg-soft-cream py-4 px-32`}
+    >
+      <Marquee>
+        <div className="flex items-center gap-8">
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+          <p>Ambillah Peranmu</p>
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default Tagline;
