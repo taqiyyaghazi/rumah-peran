e799a861936d4d3922ef12a3e2f02ec4eeab156f
taqiyyaghazi <taqiyyaghazi@gmail.com>
2024-01-07 11:24:03 +0700

    feat(CtaBox): add responsive

--- src/components/CtaBox.tsx
+++ src/components/CtaBox.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 const CtaBox = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   return (
-    <div className="w-full flex justify-center -mb-32">
-      <div className="bg-rumah-peran-red p-8 rounded-lg max-w-4xl min-h-44 flex-1">
+    <div className="w-5/6 lg:w-full flex mx-auto justify-center -mb-32">
+      <div className="bg-rumah-peran-red p-8 rounded-lg max-w-4xl 2xl:max-w-screen-2xl min-h-44 flex-1">
         {children}
       </div>
     </div>
