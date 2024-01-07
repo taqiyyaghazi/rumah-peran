b0309ad1e7f76134a4139d02a97208ee43ab46d6
taqiyyaghazi <taqiyyaghazi@gmail.com>
2024-01-07 21:35:10 +0700

    style(button): fix responsive style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -27,7 +27,7 @@ const Button = ({
     case ButtonType.outlined:
       return (
         <button
-          className={`${gilroy.className} ${className} font-bold px-6 py-4 rounded-full border border-rumah-peran-red text-rumah-peran-red`}
+          className={`${gilroy.className} ${className} max-sm:text-sm font-bold px-3 py-2 md:px-6 md:py-4 rounded-full border border-rumah-peran-red text-rumah-peran-red`}
           {...rest}
         >
           {children}
@@ -38,9 +38,8 @@ const Button = ({
         <button
           className={`${
             gilroy.className
-          } ${className} font-bold px-6 py-4 rounded-full ${
-            color === ColorType.primary &&
-            'text-soft-cream bg-rumah-peran-red'
+          } ${className} max-sm:text-sm font-bold px-3 py-2 md:px-6 md:py-4 rounded-full ${
+            color === ColorType.primary && 'text-soft-cream bg-rumah-peran-red'
           } ${
             color === ColorType.secondary &&
             'bg-soft-cream text-rumah-peran-red'
