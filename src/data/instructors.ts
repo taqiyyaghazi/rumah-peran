744c37692e18b979cddb1a70ad50cbfdacc7ef44
taqiyyaghazi <taqiyyaghazi@gmail.com>
2024-01-14 10:53:16 +0700

    feat: update data instructor

--- src/data/instructors.ts
+++ src/data/instructors.ts
@@ -6,7 +6,7 @@ export const instructors: IInstructor[] = [
     name: 'Raymond Handaya',
     title: 'Film Director & Produser',
     desc: 'Sutradara dan Produser Film di Indonesia, hingga saat ini sudah terlibat dalam banyak produksi film televisi, series, iklan, dan lebih dari 45 produksi film layar lebar. Diantaranya adalah Teka Teki Tika (2021), Imperfect (2019), Kapal Goyang Kapten (2019) dll.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/raymondhandaya',
     image: '/raymond-handaya.png',
   },
   {
@@ -14,7 +14,7 @@ export const instructors: IInstructor[] = [
     name: 'Yaya Unru',
     title: 'Aktor',
     desc: 'Merupakan aktor senior, pemain teater, sutradara, dan pelatih akting untuk project film. Beliau juga pernah mendapatkan penghargaan sebagai Pemeran Pendukung Pria terbaik di ajang FFI 2014 (Tabula Rasa) & Pemeran Pria terbaik FFI 2017 (Posesif).',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/yayuunru',
     image: '/yaya-unru.png',
   },
   {
@@ -22,7 +22,7 @@ export const instructors: IInstructor[] = [
     name: 'Ari Kusuma',
     title: 'Pengajar Akting',
     desc: 'Alumni Seni Peran Institut Kesenian Jakarta. Berkarir di Dunia panggung teater menjadi aktor, sutradara dan penulis.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/ari_kusuma81',
     image: '/ari-kusuma.png',
   },
   {
@@ -30,7 +30,7 @@ export const instructors: IInstructor[] = [
     name: 'Kameo Lova',
     title: 'Pengajar Akting',
     desc: 'Alumni Institut Kesenian Jakarta. Berkarya di panggung teater sebagai aktor, sutradara, pelatih akting film, dan pengajar teater.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/kameolova',
     image: '/kameo-lova.png',
   },
   {
@@ -38,7 +38,7 @@ export const instructors: IInstructor[] = [
     name: 'Umroh Pasaribu',
     title: 'Pengajar Akting',
     desc: 'Pengajar seni peran yang memiliki pengalaman mengajar lebih dari 6 tahun dan memiliki beberapa prestasi dalam lomba film festival baik sebagai pembina dan pemain.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/umroh_pasaribu27',
     image: '/umroh-pasaribu.png',
   },
   {
@@ -46,7 +46,7 @@ export const instructors: IInstructor[] = [
     name: 'Josevanie Looraine',
     title: 'Pengajar Sanggar Anak',
     desc: 'Alumni Rumah Peran Batch 16 yang berpengalaman mengajar ballet anak usia 4-12 tahun dan telah mendapatkan sertifikasi sebagai Penari Tari modern dari LSK TMI.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/josevanieallestra',
     image: '/josevanie-looraine.png',
   },
   {
@@ -54,7 +54,7 @@ export const instructors: IInstructor[] = [
     name: 'Stephanie Fenuela',
     title: 'Pengajar Sanggar Anak',
     desc: 'Alumni Rumah Peran Batch 12 yang berpengalaman mengajar kelas anak usia 4-12 tahun. Aktif berperan di beberapa series/film sebagai pemeran pendukung sekaligus merupakan General Manager di Rumah Peran.',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/stephaniefenuela',
     image: '/stephanie-fenuela.png',
   },
 ];
